8229c9c1a8b561a6c4a87948dfebd99df4959a45
Alexander Goncharuk <alexander.goncharuk@brainbase.com>
2021-10-10 12:35:39 +0300

    feat(tickets): add other module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'tickets', loadChildren: () => import('./tickets/tickets.module').then(m => m.TicketsModule) }];
+const routes: Routes = [{ path: 'tickets', loadChildren: () => import('./tickets/tickets.module').then(m => m.TicketsModule) }, { path: 'other', loadChildren: () => import('./other/other.module').then(m => m.OtherModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
